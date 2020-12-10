71a63d2dedd19b7714bab3e94148cc538d527047
Han Truong <hantruong996@gmail.com>
2020-12-10 16:41:57 -0500

    add in electoral votes scraping and new scatterplot

--- app.R
+++ app.R
@@ -11,15 +11,27 @@ us_states <- map_data("state") %>% mutate(state=region)
 presidential <- read.csv("Data/1976-2016-president.csv")
 context <- read.csv("Data/election-context-2018.csv")
 budg_all <- read.csv("Data/budget_amount.csv")
+electoral_votes <- read.csv('Data/Electoral_Votes_by_State.csv')
 
+turnout <- read.csv("Data/Turnout.csv") %>%
+  filter(X %in% seq(1976, 2016, by = 4)) %>%
+  select(X, State, X.4) %>% 
+  rename(year=X,state=State, turnout=X.4)
+turnout_1976 <- read.csv("Data/1976_Turnout.csv")
+turnout_2016 <- read.csv("Data/2016_Turnout.csv") %>% mutate(State=recode(State,!!!c("Dist. of Columbia"="District of Columbia")))
+presidential_turnout <- turnout_1976 %>% 
+  rename(year=Year,state=State,turnout=Turnout) %>% rbind(turnout) %>% 
+  rbind(turnout_2016 %>% rename(year=Year,state=State,turnout=Turnout)) %>%
+  mutate(year = as.numeric(as.character(year)),turnout=as.numeric(str_replace(turnout,"%",""))) %>%
+  filter(state !='United States')
 
-presidential_turnout <- presidential %>%
-  group_by(year, state) %>%
-  filter(row_number()==1) %>%
-  dplyr::select(year, state, totalvotes) 
 
 presidential_turnout_w_map <- presidential_turnout %>%
   mutate(state = tolower(state)) %>% left_join(us_states,by='state')
+
+overall_turnout <- presidential_turnout %>%
+  group_by(year) %>%
+  summarize(total = mean(turnout))
 
 parties_pres <- presidential %>%
   group_by(year,state) %>%
@@ -31,11 +43,6 @@ parties_pres_w_map <- parties_pres %>%
   mutate(state = tolower(state)) %>% left_join(us_states,by='state')
 
 
-overall_turnout <- presidential_turnout %>%
-  group_by(year) %>%
-  summarize(total = sum(totalvotes))
-
-
 margin_pres <- presidential %>%
   group_by(year,state) %>%
   arrange(desc(candidatevotes), .by_group = TRUE) %>%
@@ -45,6 +52,10 @@ margin_pres <- presidential %>%
 margin_pres_w_map <- margin_pres %>%
   mutate(state = tolower(state)) %>% left_join(us_states,by='state')
 
+
+pres_turnout_and_margin <- presidential_turnout %>% 
+  left_join(margin_pres,by=c('state','year')) %>%
+  left_join(electoral_votes,by=c('state','year'))
 
 context_state<- context %>%
   group_by(state) %>%
@@ -96,8 +107,8 @@ state_choices <- unique(presidential_turnout$state)
 govagency_choices <- rownames(budg_admin)
 map_modes <- c('Race Result','Margins','Voter Turnout')
 
-color_scale_min <- c('Margins'= min(margin_pres$margin),'Voter Turnout'=min(presidential_turnout$totalvotes))
-color_scale_max <- c('Margins'= max(margin_pres$margin),'Voter Turnout'=max(presidential_turnout$totalvotes))
+color_scale_min <- c('Margins'= min(margin_pres$margin),'Voter Turnout'=min(presidential_turnout$turnout))
+color_scale_max <- c('Margins'= max(margin_pres$margin),'Voter Turnout'=max(presidential_turnout$turnout))
 
 agencies <- c("DOE","DOJ")
 
@@ -115,9 +126,10 @@ ui <- fluidPage(
              column(9,
                 sliderInput("year","Choose year",
                             value = 2000, min = 1976, 
-                            max = 2016,step=4,sep="",width='85%'
+                            max = 2016,step=4,sep="",width='80%'
                 ),
-                plotOutput(outputId = "election_map",height=500,width='100%')
+                plotOutput(outputId = "election_map",height=500,width='100%'),
+                align='center'
              ),
              selectizeInput("states_turnout","Select up to 6 states:",
                             choices=state_choices,
@@ -148,6 +160,11 @@ ui <- fluidPage(
                     ),
                     plotOutput(outputId = "line_votevalue",height=500,width='100%')
              ),
+             sliderInput("year_scatter","Choose year",
+                         value = 2000, min = 1976, 
+                         max = 2016,step=4,sep="",width='85%'
+             ),
+             plotOutput(outputId = "scatter",height=700,width='100%')
              
     ),
     tabPanel("Analysis",
@@ -162,11 +179,11 @@ server <- function(input, output) {
 
   mapdata <- reactive(
     if (input$mapmode =="Voter Turnout"){
-      presidential_turnout_w_map %>% filter(year==input$year) %>% mutate(fillcolor=totalvotes)
+      presidential_turnout_w_map %>% filter(year==input$year) %>% mutate(fillcolor=turnout)
     } else if (input$mapmode == "Margins"){
       margin_pres_w_map %>% filter(year==input$year) %>% mutate(fillcolor=margin)
     } else {
-      parties_pres_w_map %>% filter(year==input$year) %>% mutate(fillcolor=party)
+      parties_pres_w_map %>% filter(year==input$year) %>% mutate(fillcolor=str_to_title(party))
     }
     )
 
@@ -185,20 +202,27 @@ server <- function(input, output) {
             legend.position="bottom",
             plot.title = element_text(hjust = 0.5)
       ) 
-     if (input$mapmode %in% c('Margins','Voter Turnout')) {
+     if (input$mapmode == 'Voter Turnout') {
          p <- p + scale_fill_distiller(name=input$mapmode,
                                   palette="Spectral",
-                                  trans="log10",
                                   limits= c(color_scale_min[input$mapmode],color_scale_max[input$mapmode])
              ) + theme(legend.key.width = unit(6,"lines"))
+     } else if (input$mapmode == 'Margins'){
+       p <- p + scale_fill_distiller(name=input$mapmode,
+                                     trans='log10',
+                                     palette="Spectral",
+                                     limits= c(color_scale_min[input$mapmode],color_scale_max[input$mapmode])
+       ) + theme(legend.key.width = unit(6,"lines"))
+     } else {
+        p <- p + scale_fill_manual(values=c("Democrat"="blue","Republican"="red","Democratic-Farmer-Labor"="darkgreen"))
       }
-      p
+     p
   })
   
   line_turnout_data <- reactive(presidential_turnout %>% filter(state %in% input$states_turnout)) 
   output$line_turnout <- renderPlot({ 
     line_turnout_data() %>%
-      ggplot(aes(year,totalvotes,color=state)) + 
+      ggplot(aes(year,turnout,color=state)) + 
       geom_line() +
       geom_point() +
       ggtitle("Voter Turnout") + 
@@ -229,13 +253,13 @@ server <- function(input, output) {
     table_demo_data()
   })
   
-  line_margin_data <- reactive(margin_pres %>%  filter(state ==input$states_margin)) 
+  line_margin_data <- reactive(margin_pres %>%  filter(state ==input$states_margin) %>% mutate(party=str_to_title(party))) 
   output$line_margin <- renderPlot({ 
     line_margin_data() %>%
       ggplot(aes(year,margin)) + 
       geom_line() +
       geom_point(aes(colour = party), size = 5) +
-      scale_color_manual(values=c("blue", "red")) +
+      scale_color_manual(values=c("Democrat"="blue","Republican"="red","Democratic-Farmer-Labor"="darkgreen")) +
       ggtitle('Vote Margin') +
       theme(text = element_text(size=15),
             legend.position="bottom",
@@ -263,6 +287,31 @@ server <- function(input, output) {
             plot.title = element_text(hjust = 0.5)) +
       # increase size of the marker inside the legend label
       guides(colour = guide_legend(override.aes = list(size=9)))
+  })
+  
+  scatter_data <- reactive(pres_turnout_and_margin %>% filter(year==input$year_scatter) %>% 
+                           left_join(influence,by='state') %>% 
+                           mutate(party=str_to_title(party))
+                           )
+  output$scatter <- renderPlot({
+    scatter_data() %>%
+      ggplot(aes(margin, turnout, color = party, size=nvotes)) +
+      geom_point(alpha = 0.5) +
+      xlab("Margin") +
+      ylab("Turnout") +
+      scale_x_log10(limits=c(color_scale_min['Margins'],color_scale_max['Margins'])) +
+      ylim(c(color_scale_min['Voter Turnout'],color_scale_max['Voter Turnout'])) +
+      # Set x and y axis limits to keep the same for each year
+      # Make the legend titles nicer
+      scale_color_manual(values=c("Democrat"="blue","Republican"="red","Democratic-Farmer-Labor"="darkgreen")) +
+      scale_size_continuous(name = "# of Electoral Votes",range=c(5,30)) +
+      
+      # Change the title of the plot for each year
+      # Returns a character vector containing a formatted combination 
+      # of text and variable values
+      ggtitle(sprintf("Turnout vs Margin in %d", input$year_scatter)) +
+      theme_bw() + 
+      theme(text = element_text(size=20))
   })
 
   
